8232f817fe6898410d67131468606f75f50d3c22
pirbhai14 <pirbhai14@hotmail.com>
2022-12-10 00:31:21 +0400

    Day 31: enable only sations and also temp for whole yr

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ def home():
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
     filename = 'Data\TG_STAID' + station.zfill(6) + '.txt'
-    type_var = type(station)
     df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
     temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze()/10
     return {'station': station, 
@@ -23,6 +22,20 @@ def about(station, date):
             'temperature': temperature
             }
 
+@app.route("/api/v1/<station>")
+def station_only(station):
+    filename = 'Data\TG_STAID' + station.zfill(6) + '.txt'
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    return df.to_dict(orient='records')
+
+
+@app.route("/api/v1/yearly/<station>/<year1>")
+def station_year(station, year1):
+    filename = 'Data\TG_STAID' + station.zfill(6) + '.txt'
+    df = pd.read_csv(filename, skiprows=20)
+    df['    DATE'] = df['    DATE'].astype(str)
+    result = df.loc[df['    DATE'].str.startswith(str(year1))]
+    return result.to_dict(orient='records')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
